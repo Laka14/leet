4c6f519d00e58ca6cfdf713fec37c07ded70b68b
Shrinath Yadav Laka <97254409+Laka14@users.noreply.github.com>
2025-01-25 23:39:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (25.52%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -10,8 +10,9 @@ public:
         for(int i =n-1; i>0; i--){
             suf[i-1] = suf[i]*nums[i];
         }
-        for(int i =0; i<nums.size(); i++) nums[i] =pre[i]*suf[i];
-        
-        return nums;
+        vector<int> ans(n);
+        for(int i =0; i<nums.size(); i++) ans[i] =pre[i]*suf[i];
+
+        return ans;
     }
 };
